a6a10378f36dda204c4a40f8f8a4a8d3f804774f
Rishab <rishab@Rishabs-MacBook-Pro.local>
2015-02-17 12:31:05 +0530

    Initial Commit

---
+++ CircularProgressViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircularProgressViewDemo
+//
+//  Created by Rishab on 17/02/15.
+//  Copyright (c) 2015 Rishab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircularProgressViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircularProgressViewDemo
+//
+//  Created by Rishab on 17/02/15.
+//  Copyright (c) 2015 Rishab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
